f85aeabc904d1914520c2f3944edc934ea225d9d
HAKERCRAFT2077 <79506370+haker20SZs@users.noreply.github.com>
2021-03-15 17:17:39 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,7 +18,8 @@ menu()
 	echo "- 6 - Устоновить толка автозапуск"
 	echo "- 7 - Удалить толка автозапуск"
 	echo "- 8 - Удалить всё автозапуск и сервер"
-	echo "- 9 - Выход"
+	echo "- 9 - Обновить скрипт автоустановки"
+	echo "- 0 - Выход"
 	read -p "Пожалуйста, введите пункт меню: " case
 	case $case in
 		1) cd /home/ && mkdir srv && cd /home/srv/ && wget "https://hakercraft.ru/srv/hakercraft.ru.tar.gz" && tar -xvf hakercraft.ru.tar.gz && chmod 777 /home/srv/start.sh && chmod 777 /home/srv/bin/php7/bin/php && rm /home/srv/hakercraft.ru.tar.gz && cd /root && cd /etc/ && wget "https://hakercraft.ru/srv/rc.local" && chmod +x /etc/rc.local && cd /etc/systemd/system/ && wget "https://hakercraft.ru/srv/rc-local.service" && systemctl enable rc-local && systemctl start rc-local;; 
@@ -29,7 +30,9 @@ menu()
 		6) cd /etc/ && wget "https://hakercraft.ru/srv/rc.local" && chmod +x /etc/rc.local && cd /etc/systemd/system/ && wget "https://hakercraft.ru/srv/rc-local.service" && systemctl enable rc-local && systemctl start rc-local;;
 		7) rm -R /etc/rc.local && rm -R /etc/systemd/system/rc-local.service;;
 		8) rm -R /home/srv && rm -R /etc/rc.local && rm -R /etc/systemd/system/rc-local.service;;
-		9) exit;;
+		9) apt-get update && apt-get upgrade && wget https://github.com/haker20SZs/minecraft_server/blob/main/README.md
+		0) exit;;
 	esac
 }
 menu
+
